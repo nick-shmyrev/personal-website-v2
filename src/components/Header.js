fc0445be068cdd18fd318ebcc02078e7cb972e28
Nick Shmyrev <nr.shmyrev@gmail.com>
2018-07-28 17:08:50 -0400

    Added Header markup

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,32 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Personal Website v2</h1>
-    <hr/>
-    <NavLink exact to="/" activeClassName="navlink-active">Home Page</NavLink>
+    <div className="header__menu-btn">
+      <div className="header__btn-line"/>
+      <div className="header__btn-line"/>
+      <div className="header__btn-line"/>
+    </div>
+    
+    <nav>
+      <div className="header__menu-branding">
+        <div className="portrait"/>
+      </div>
+      
+      <ul className="header__menu-nav">
+        <li className="header__nav-item">
+          <NavLink exact to="/" className="header__nav-link" activeClassName="header__nav-link--active">Home</NavLink>
+        </li>
+        <li className="header__nav-item">
+          <NavLink to="/about" className="header__nav-link" activeClassName="header__nav-link--active">About Me</NavLink>
+        </li>
+        <li className="header__nav-item">
+          <NavLink to="/portfolio" className="header__nav-link" activeClassName="header__nav-link--active">Examples of My Work</NavLink>
+        </li>
+        <li className="header__nav-item">
+          <NavLink to="/contact" className="header__nav-link" activeClassName="header__nav-link--active">Contact Me</NavLink>
+        </li>
+      </ul>
+    </nav>
   </header>
 );
 
