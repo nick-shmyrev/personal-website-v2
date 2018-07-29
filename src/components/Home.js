4fc9ac1f4db05644cbfb19c7735b19fdbbef26ee
Nick Shmyrev <nr.shmyrev@gmail.com>
2018-07-28 20:42:56 -0400

    Completed Home component markup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => (
-  <div>
-    <h2>Home Page</h2>
-    <p>Home page component placeholder</p>
-  </div>
+  <main id="home">
+    <h1 className="lg-heading">Nick <span className="text-secondary">Shmyrev</span></h1>
+    <h2 className="sm-heading">Front-End Developer | JavaScript Enthusiast & Just A Nice Person</h2>
+    
+    <div className="social-icons">
+      <a href="https://www.linkedin.com/in/nickshmyrev/" target="_blank" rel="noopener noreferrer" className="icons">
+        <FontAwesomeIcon icon={faLinkedin} className="fa-2x"/>
+      </a>
+      <a href="https://github.com/nick-shmyrev" target="_blank" rel="noopener noreferrer" className="icons">
+        <FontAwesomeIcon icon={faGithub} className="fa-2x"/>
+      </a>
+    </div>
+  </main>
 );
 
 export default Home;
