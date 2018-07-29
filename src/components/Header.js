92d196de35d1cd4007050f4e26cc7bd89eb5ee1e
Nick Shmyrev <nr.shmyrev@gmail.com>
2018-07-28 22:36:13 -0400

    Added menu button animation & styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <div className="header__menu-btn">
-      <div className="header__btn-line"/>
-      <div className="header__btn-line"/>
-      <div className="header__btn-line"/>
-    </div>
+class Header extends Component {
+  state = {
+    showMenu: false,
+  };
+  
+  toggleMenu = () => {
+    this.setState(prevState => ({ showMenu: !prevState.showMenu }));
+  };
+  
+  render() {
+    return (
+      <header>
+        <div
+          className={`menu-btn ${this.state.showMenu ? 'close' : ''}`}
+          onClick={this.toggleMenu}
+        >
+          <div className="btn-line"/>
+          <div className="btn-line"/>
+          <div className="btn-line"/>
+        </div>
     
-    <nav>
-      <div className="header__menu-branding">
-        <div className="portrait"/>
-      </div>
+        <nav className={`menu ${this.state.showMenu ? 'show' : ''}`}>
+          <div className={`menu-branding ${this.state.showMenu ? 'show' : ''}`}>
+            <div className="portrait"/>
+          </div>
       
-      <ul className="header__menu-nav">
-        <li className="header__nav-item">
-          <NavLink exact to="/" className="header__nav-link" activeClassName="header__nav-link--active">Home</NavLink>
-        </li>
-        <li className="header__nav-item">
-          <NavLink to="/about" className="header__nav-link" activeClassName="header__nav-link--active">About Me</NavLink>
-        </li>
-        <li className="header__nav-item">
-          <NavLink to="/portfolio" className="header__nav-link" activeClassName="header__nav-link--active">Examples of My Work</NavLink>
-        </li>
-        <li className="header__nav-item">
-          <NavLink to="/contact" className="header__nav-link" activeClassName="header__nav-link--active">Contact Me</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+          <ul className={`menu-nav ${this.state.showMenu ? 'show' : ''}`}>
+            <li className={`nav-item ${this.state.showMenu ? 'show' : ''}`}>
+              <NavLink exact to="/" className="nav-link" activeClassName="nav-link--active">Home</NavLink>
+            </li>
+            <li className={`nav-item ${this.state.showMenu ? 'show' : ''}`}>
+              <NavLink to="/about" className="nav-link" activeClassName="nav-link--active">About Me</NavLink>
+            </li>
+            <li className={`nav-item ${this.state.showMenu ? 'show' : ''}`}>
+              <NavLink to="/portfolio" className="nav-link" activeClassName="nav-link--active">Examples of My Work</NavLink>
+            </li>
+            <li className={`nav-item ${this.state.showMenu ? 'show' : ''}`}>
+              <NavLink to="/contact" className="nav-link" activeClassName="nav-link--active">Contact Me</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+};
 
 export default Header;
