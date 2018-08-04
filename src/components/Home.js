9e40b2cd513629486dc9db7df98f678a990f6a7e
Nick Shmyrev <nr.shmyrev@gmail.com>
2018-08-04 13:36:58 -0400

    Added gitlab link to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faGitlab } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => (
   <main id="home">
@@ -10,6 +10,9 @@ const Home = () => (
     <div className="social-icons">
       <a href="https://www.linkedin.com/in/nickshmyrev/" target="_blank" rel="noopener noreferrer" className="icons">
         <FontAwesomeIcon icon={faLinkedin} className="fa-2x"/>
+      </a>
+      <a href="https://gitlab.com/nick-shmyrev" target="_blank" rel="noopener noreferrer" className="icons">
+        <FontAwesomeIcon icon={faGitlab} className="fa-2x"/>
       </a>
       <a href="https://github.com/nick-shmyrev" target="_blank" rel="noopener noreferrer" className="icons">
         <FontAwesomeIcon icon={faGithub} className="fa-2x"/>
