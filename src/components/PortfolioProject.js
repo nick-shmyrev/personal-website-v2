52f100774c9a5a42db9bb4209fb8156f58473ae2
Nick Shmyrev <nr.shmyrev@gmail.com>
2018-08-06 17:40:39 -0400

    Added image hover effect with project title and description

--- src/components/PortfolioProject.js
+++ src/components/PortfolioProject.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 const PortfolioProject = (props) => {
-  const project = props.project;
+  const { project } = props;
   return (
-    <div className="project">
-      <a href={project.url ? project.url : '#'} target="_blank">
+    <div className="project-item">
+      <a href={project.url ? project.url : '#'} className="project-image" target="_blank">
         <img src={project.img} alt={project.name}/>
-      </a>
-      <a href={project.url ? project.url : '#'} className="btn btn-light" target="_blank">
-        <FontAwesomeIcon icon={faEye} className="fab"/> {project.name}
+        <span className="overlay">
+          <h4>{project.name}</h4>
+          <p>{project.description}</p>
+        </span>
       </a>
       <a href={project.git ? project.git : '#'} className="btn btn-dark" target="_blank">
         <FontAwesomeIcon icon={faGithub} className="fab"/> GitHub
